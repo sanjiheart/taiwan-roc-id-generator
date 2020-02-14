c36ae40eb78addca8515e0b4c6ba5fda8230dd5a
sanjiheart <kaungzai@gmail.com>
2020-02-14 23:45:38 +0800

    Fix bug of final digit

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ $(function () {
         let n8 = Math.floor(Math.random() * 9) + 1;
         let n9 = Math.floor(Math.random() * 9) + 1;
         let n10 = Math.floor(Math.random() * 9) + 1;
-        let n11 = 10 - (n1 * 1 + n2 * 9 + n3 * 8 + n4 * 7 + n5 * 6 + n6 * 5 + n7 * 4 + n8 * 3 + n9 * 2 + n10 * 1) % 10;
+        // mod 10 to get units digit
+        let n11 = (10 - (n1 * 1 + n2 * 9 + n3 * 8 + n4 * 7 + n5 * 6 + n6 * 5 + n7 * 4 + n8 * 3 + n9 * 2 + n10 * 1) % 10) % 10;
         let result = cityCodeNum[0] + String(n3) + String(n4) + String(n5) + String(n6) + String(n7) + String(n8) + String(n9) + String(n10) + String(n11);
         $('#result').val(result);
     });
